e45e2694777fb5c922d8629c64a46305e710743e
Myka Kugaya <mykakug129@gmail.com>
2020-05-24 15:34:29 -0700

    get and post methods working

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+//Makes sure CSS loads with Notes page
+app.use(express.static("public"));
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
