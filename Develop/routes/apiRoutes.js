23b1b2d428247aa0b8298113c184863a28a5e737
Myka Kugaya <mykakug129@gmail.com>
2020-05-25 22:15:30 -0700

    delete method working, db file updated with post and delete methods

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -8,14 +8,27 @@ const dbPath = path.join(db_dir, "db.json");
 module.exports = function(app) {
     // API GET Request
     app.get("/api/notes", function(req, res) {
-      res.json(notes);
+        //Read db.json file and return notes array
+        fs.readFile(dbPath, 'utf8', function(err, data) {
+            if(err) {
+                throw err;
+            } else {
+                let notesArr = JSON.parse(data);
+                return res.json(notesArr);
+            }
+        })
     });
 
     // API POST Request
     app.post("/api/notes", function(req, res) {
+        //Assign ID and push new note
         req.body.id = `${notes.length}`;
         notes.push(req.body);
-        res.json(true);
+        //Rewrite updated notes to db file
+        fs.writeFile(dbPath, JSON.stringify(notes, null, '\t'), 'utf8', function(err) {
+            if (err) throw err;
+        })
+        return res.json(true);
     });
 
     //API DELETE Request
@@ -25,14 +38,14 @@ module.exports = function(app) {
             if (deletedId === notes[i].id) {
                 //Create new notes array without selected ID object
                 let newNotes = notes.filter(item => item.id !== deletedId);
-                // notes.splice(deletedId, 1);
-
                 //Reassign IDs
                 for (let j=0; j<newNotes.length; j++) {
-                    newNotes[j].id = j;
-                }
+                    newNotes[j].id = JSON.stringify(j);
+                };
                 //Rewrite new notes to db file
-                fs.writeFile(dbPath, newNotes);
+                fs.writeFile(dbPath, JSON.stringify(newNotes, null, '\t'), 'utf8', function(err) {
+                    if (err) throw err;
+                });
                 return res.json(true);
             }
         };
