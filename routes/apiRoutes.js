966da385b94887f203c2d1d139943ac07f064a09
Myka Kugaya <mykakug129@gmail.com>
2020-05-26 15:14:56 -0700

    fixed delete and post methods

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,35 +21,49 @@ module.exports = function(app) {
 
     // API POST Request
     app.post("/api/notes", function(req, res) {
-        //Assign ID and push new note
-        req.body.id = `${notes.length}`;
-        notes.push(req.body);
-        //Rewrite updated notes to db file
-        fs.writeFile(dbPath, JSON.stringify(notes, null, '\t'), 'utf8', function(err) {
-            if (err) throw err;
+        fs.readFile(dbPath, 'utf8', function(err, data) {
+            if(err) {
+                throw err;
+            } else {
+                let notesArr = JSON.parse(data);
+                //Assign ID and push new note
+                req.body.id = `${notesArr.length}`;
+                notesArr.push(req.body);
+                //Rewrite updated notes to db file
+                fs.writeFile(dbPath, JSON.stringify(notesArr, null, '\t'), 'utf8', function(err) {
+                    if (err) throw err;
+                })
+                return res.json(true);
+            }
         })
-        return res.json(true);
     });
 
     //API DELETE Request
     app.delete("/api/notes/:id", function(req, res) {
         const deletedId = req.params.id;
-        for (let i = 0; i < notes.length; i++) {
-            if (deletedId === notes[i].id) {
-                //Create new notes array without selected ID object
-                let newNotes = notes.filter(item => item.id !== deletedId);
-                //Reassign IDs
-                for (let j=0; j<newNotes.length; j++) {
-                    newNotes[j].id = JSON.stringify(j);
+        fs.readFile(dbPath, 'utf8', function(err, data) {
+            if(err) {
+                throw err;
+            } else {
+                let notesArr = JSON.parse(data);
+                for (let i = 0; i < notesArr.length; i++) {
+                    if (deletedId === notesArr[i].id) {
+                        //Create new notes array without selected ID object
+                        let newNotes = notesArr.filter(item => item.id !== deletedId);
+                        //Reassign IDs
+                        for (let j=0; j<newNotes.length; j++) {
+                            newNotes[j].id = JSON.stringify(j);
+                        };
+                        //Rewrite new notes to db file
+                        fs.writeFile(dbPath, JSON.stringify(newNotes, null, '\t'), 'utf8', function(err) {
+                            if (err) throw err;
+                        });
+                        return res.json(true);
+                    }
                 };
-                //Rewrite new notes to db file
-                fs.writeFile(dbPath, JSON.stringify(newNotes, null, '\t'), 'utf8', function(err) {
-                    if (err) throw err;
-                });
-                return res.json(true);
+                // If id doesn't exist, return false
+                return res.json(false);
             }
-        };
-        // If id doesn't exist, return false
-        return res.json(false);
+        })
     });
 };
